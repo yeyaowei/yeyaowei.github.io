8fc8e35c57ff335f286c79ba5c126c464ac2431f
yeyaowei <yeyaowei@rhw-team.com>
2019-08-31 17:17:39 +0800

    更新依赖 & 使用 Circle CI 部署服务 这背后暗藏着无数被我清掉的 commit ...

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
 export TZ='Asia/Shanghai'
-
+git config --global user.name "yeyaowei"
+git config --global user.email "yeyaowei@rhw-team.com"
 # 先 clone 再 commit，避免直接 force commit
 git clone -b master "git@github.com:yeyaowei/yeyaowei.github.io.git" .deploy_git
 
@@ -9,10 +10,9 @@ cd .deploy_git
 git checkout master
 mv .git/ ../public/
 cd ../public
-
 git add .
-git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
+git commit -m "更新: `date +"%Y-%m-%d %H:%M:%S"`"
 
 # 推送
 git push "git@github.com:yeyaowei/yeyaowei.github.io.git" master:master --force --quiet
-git push "git@yeyaowei.xyz:blog.git" master:master --force --quiet
+# git push "git@yeyaowei.xyz:blog.git" master:master --force --quiet
