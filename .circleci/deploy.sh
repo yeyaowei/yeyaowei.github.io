19fc389690fb6cddd7cb62c04a382d8318ad2e20
yeyaowei <yeyaowei@rhw-team.com>
2019-09-01 23:27:59 +0800

    整理部署配置

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -ev
 export TZ='Asia/Shanghai'
+
 git config --global user.name "yeyaowei"
 git config --global user.email "yeyaowei@rhw-team.com"
-# 先 clone 再 commit，避免直接 force commit
+
 git clone -b master "git@github.com:yeyaowei/yeyaowei.github.io.git" .deploy_git
 
 cd .deploy_git
 git checkout master
+
 mv .git/ ../public/
+
+# 复制 Circle CI 配置到 master 分支
 mkdir ../public/.circleci
 cp ../.circleci/config.yml ../public/.circleci
+
 cd ../public
 git add .
 git commit -m "更新: `date +"%Y-%m-%d %H:%M:%S"`"
